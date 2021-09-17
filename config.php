6cf709f1257bbd5a2718c2d8a1c2e3accb3fb71e
Mikail Yusuf <mikailkyusuf.com>
2021-09-17 12:09:57 +0100

    commit

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 $servername = "us-cdbr-east-04.cleardb.com";
 $username = "b3b7a278827dcb";
 $password = "bd4020e6";
-$dbname = "foodie";
+$dbname = "heroku_556bf5eabda18a8";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
-    die("Connection failed: bla bla bla" . mysqli_connect_error());
+    die("Connection failed:" . mysqli_connect_error());
 }
 else{
     // echo ("SUCCESS");
