a099751487bc80a093492eda0a4f9831f8163238
Mikail Yusuf <mikailkyusuf.com>
2021-09-17 12:01:29 +0100

    make it better

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ $password = "root";
 $dbname = "foodie";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+    die("Connection failed: bla bla bla" . mysqli_connect_error());
 }
 else{
     // echo ("SUCCESS");
