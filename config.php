a5337bf7979ebe440091bc4a73df540fda833bec
Mikail Yusuf <mikailkyusuf.com>
2021-09-17 08:31:44 +0100

    code refactor

--- config.php
+++ config.php
@@ -7,6 +7,7 @@ $conn = mysqli_connect($servername, $username, $password, $dbname);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
+//show error
 else{
     // echo ("SUCCESS");
 }
