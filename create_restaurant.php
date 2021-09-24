a74fcfa88eb4205e1c6f424eb69d31954805a459
Mikail Yusuf <mikailkyusuf.com>
2021-09-24 19:17:59 +0100

    code refactor

--- create_restaurant.php
+++ create_restaurant.php
@@ -1,52 +1,158 @@
 <?php
 include 'config.php';
 include 'token_generator.php';
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require 'vendor/autoload.php';
 
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 if($requestMethod == "POST"){
-try{
-$json_string = file_get_contents( "php://input");
-$data = json_decode($json_string,true);
+ try{
+ $json_string = file_get_contents( "php://input");
+ $data = json_decode($json_string,true);
 
-if(sizeof($data) > 4){
+    if(sizeof($data) > 4){
 		$name=$data['name'];
 		$email=$data['email'];
 		$phone=$data['phone'];
 		$address=$data['address'];
-        $password= password_hash($data['password'], PASSWORD_DEFAULT);
-        $token = generateToken();
-        $user_id = uniqid();
+    $password = md5(check_input($data['password']));
+    
+    $status=0;
+    $activationcode=md5($email.time());
 
-        $sql = "INSERT INTO `restaurants`( `id`,`name`, `email`,`phone`,`address`,`token`,`password`) 
-		VALUES ('$user_id','$name','$email','$phone','$address','$token','$password')";
-		if (mysqli_query($conn, $sql)) {
-            echo sizeof($data);
-            $message = json_encode(array("message" => "Restaurant Created Successfully", "status" => true));	
-            http_response_code(201);
-            echo $message;
-		} 
-		else {
-		    
-            $message = json_encode(array("message" => mysqli_error($conn), "status" => false));	
-            http_response_code(400);
-            echo $message;
-		}
-      }
-      else{
+    verifyEmail($email);
+              
+  }
+     else{
         http_response_code(400);
     } 
-}   
-
-catch(Exception $e) {
-    echo 'Message: ' .$e->getMessage();
   }
 
-}
+    catch(Exception $e){
+      $message = json_encode(array("message" => "Bad Request $e", "status" => false));	
+                http_response_code(400);
+                echo $message;
+    }
+   
+}  
 
-else{
+else {
   $message = json_encode(array("message" => "Bad Request", "status" => false));	
             http_response_code(400);
             echo $message;
+}
+
+
+function sendMail($email,$username){
+
+  $message = "
+				<html>
+				<head>
+				<title>Verification Code</title>
+				</head>
+				<body>
+				<h2>Thank you for Registering.</h2>
+				<p>Your Account:</p>
+				<p>Email: ".$email."</p>
+		
+				<p>Please click the link below to activate your account.</p>
+				<h4><a href='http://localhost/email_verification.php?uid=$uid&code=$code'>Activate My Account</h4>
+				</body>
+				</html>
+				";
+
+ $mail = new PHPMailer(true);
+
+ try {
+    //Server settings
+    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+    $mail->isSMTP();                                            //Send using SMTP
+    $mail->Host       = 'smtp.example.com';                     //Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+    $mail->Username   = 'devmikaeelkyusuf@gmail.com';                     //SMTP username
+    $mail->Password   = 'mikail.kunle';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+    $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+
+    //Recipients
+    $mail->setFrom('from@example.com', 'Mailer');
+    $mail->addAddress('joe@example.net', 'Joe User');     //Add a recipient
+    $mail->addAddress('ellen@example.com');               //Name is optional
+    $mail->addReplyTo('devmikaeelkyusuf@gmail.com', 'Information');
+   
+    //Content
+    $mail->isHTML(true);                                  //Set email format to HTML
+    $mail->Subject = 'Email Verification';
+    $mail->Body    = $message;
+    //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+  }
+ catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+
+function verifyEmail($email){
+
+  $token = bin2hex(random_bytes(50));
+
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    http_response_code(401);
+    $message = json_encode(array("message" => "Email not valid","status" => false));	
+    echo $message;
+  }
+  else{
+    $query=mysqli_query($conn,"select * from user where email='$email'");
+		if(mysqli_num_rows($query)>0){
+      http_response_code(401);
+      $message = json_encode(array("message" => "Email already taken","status" => false));	
+      echo $message;
+		}
+		else{
+      registerUser();
+    }
+
+  }
+	
+}
+
+function registerUser(){
+
+try{
+    $sql = "INSERT INTO `restaurants`( `user_id`,`name`, `email`,`phone`,`is_verified`,`is_active`,`address`,`token`,`password`) 
+    VALUES ('$uniqid()','$name','$email','$phone','$address','$token','$password')";
+
+    if (mysqli_query($conn, $sql)) {
+
+      http_response_code(201);
+      $message = json_encode(array("message" => "User Created","status" => true));	
+      echo $message;
+      sendMail($email, $name);
+
+    }
+            
+    
+    else {
+      $message = json_encode(array("message" => "mysqli_error($conn)", "status" => false));	
+      http_response_code(400);
+
+     }
+ }
+
+  catch (Exception $e){
+
+  http_response_code(400);
+  $message = json_encode(array("message" => "Error message = $e","status" => false));	
+  echo $message;
+
+  }
+
 }
 
 ?>
--- login.php
+++ login.php
@@ -1,43 +1,51 @@
 <?php
 include 'config.php';
+ 
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+ 
+        $json_string = file_get_contents( "php://input");
+        $data = json_decode($json_string,true); 
 
-try{
-$json_string = file_get_contents( "php://input");
-$data = json_decode($json_string,true);
+		$email=check_input($data['email']);
+		$password=md5(check_input($data['password']));
+ 
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+              http_response_code(401);
+              $message = json_encode(array("message" => "Invalid email format","status" => false));	
+              echo $message;
+		}
+		else{
+			$query=mysqli_query($conn,"select * from restaurants where email='$email' and password='$password'");
+			if(mysqli_num_rows($query)==0){
+                http_response_code(401);
+                $message = json_encode(array("message" => "User not found","status" => false));	
+                echo $message;
+			}
+			else{
+				$row=mysqli_fetch_array($query);
+				if($row['verify']==0){
+                    http_response_code(401);
+                    $message = json_encode(array("message" => "Account not Verified","status" => false));	
+                    echo $message;
+                }
+				else{
+                  //  show user Id =$row['userid'];
+                  http_response_code(200);
+                  $message = json_encode(array("message" => "User Id is $row['userid'] ","status" => false));	
+                  echo $message;
+				}
+			}
+		}
+ 
+	}
 
-		$emailData=stripcslashes($data['email']);
-        $email = mysqli_real_escape_string($conn, $emailData);
-		$passwordData=$data['password'];
-        $password = mysqli_real_escape_string($conn, $passwordData);
-
-        $sql = "SELECT * FROM users WHERE email = '$email' ";  
-        $result = mysqli_query($conn, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
-        $count = mysqli_num_rows($result);
-
-    
-        if($email == $row['email']){
-      
-        if (password_verify($password, $row['password'])) {
-            $message = json_encode(array("message" => "Login Success ","token"=>$row['email'], "status" => true));	
-            http_response_code(200);
-            echo $message;
-        } else {
-            http_response_code(401);
-            $message = json_encode(array("message" => "Invalid Password ","status" => false));	
-            echo $message;
-        }
-    }
     else{
-        http_response_code(401);
-        $message = json_encode(array("message" => "Invalid Username or Password ","status" => false));	
-        echo $message;
+        http_response_code(400);
+                  $message = json_encode(array("message" => "Bad request","status" => false));	
+                  echo $message;
     }
-		
-}
-catch(Exception $e) {
-    http_response_code(401);
-    echo('Message: ' .$e->getMessage());
-    $message = json_encode(array("message" => $e->getMessage(),"status" => false));	
-    echo $message;
-  }
+?>
+
+
+
+  
